76a83f280dace497eff24089b5b35652f57c1e75
j <jessfong1@yahoo.com>
2024-01-08 17:52:59 -0700

    replace unsafe GEM with javascript

--- Gemfile
+++ Gemfile
@@ -17,7 +17,6 @@ group :jekyll_plugins do
     gem 'jekyll-archives'
     gem 'kramdown'
     gem 'rouge'
-    gem 'jekyll-target-blank'
 end
 
 gem "webrick", "~> 1.7"
---
+++ assets/js/ext-link-new-tab.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function externalLinks() {
+    var anchors = document.getElementsByTagName("a");
+    for (var i = 0; i < anchors.length; i++) {
+      if (anchors[i].hostname !== window.location.hostname) {
+        anchors[i].setAttribute("target", "_blank");
+        anchors[i].setAttribute("rel", "noopener");
+      }
+    }
+  });
